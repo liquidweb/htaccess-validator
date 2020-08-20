0c47eb62765eac8cd8e66fb8123a205e2ac0076a
Steve Grunwell <sgrunwell@liquidweb.com>
2020-08-20 12:34:35 -0400

    WHITESPACE ONLY: Fix mixed spaces/tabs

--- src/Validator.php
+++ src/Validator.php
@@ -6,95 +6,95 @@ use LiquidWeb\HtaccessValidator\Exceptions\ValidationException;
 
 class Validator
 {
-	/**
-	 * @var string The file being validated.
-	 */
-	protected $file;
+    /**
+     * @var string The file being validated.
+     */
+    protected $file;
 
-	/**
-	 * @var bool Whether or not $this->file is a temp file.
-	 */
-	protected $isTempFile = false;
+    /**
+     * @var bool Whether or not $this->file is a temp file.
+     */
+    protected $isTempFile = false;
 
-	/**
-	 * Construct a new instance of the class.
-	 *
-	 * @param string $file The system path to the file being validated.
-	 */
-	public function __construct($file)
-	{
-		$this->file = (string) $file;
-	}
+    /**
+     * Construct a new instance of the class.
+     *
+     * @param string $file The system path to the file being validated.
+     */
+    public function __construct($file)
+    {
+        $this->file = (string) $file;
+    }
 
     /**
      * Automatically remove temporary files as script execution ends.
      */
-	public function __destruct()
-	{
-		if ($this->isTempFile) {
-			unlink($this->file);
-		}
-	}
+    public function __destruct()
+    {
+        if ($this->isTempFile) {
+            unlink($this->file);
+        }
+    }
 
-	/**
-	 * Retrieve the underlying filepath.
-	 *
-	 * @return string The path to $this->file.
-	 */
-	public function getFilePath()
-	{
-		return $this->file;
-	}
+    /**
+     * Retrieve the underlying filepath.
+     *
+     * @return string The path to $this->file.
+     */
+    public function getFilePath()
+    {
+        return $this->file;
+    }
 
-	/**
-	 * Mark $this->file as being temporary, indicating it should be removed after validation.
-	 *
-	 * @param bool $isTempFile Whether or not the file should be considered temporary.
-	 */
-	public function setIsTempFile($isTempFile)
-	{
-		$this->isTempFile = (bool) $isTempFile;
-	}
+    /**
+     * Mark $this->file as being temporary, indicating it should be removed after validation.
+     *
+     * @param bool $isTempFile Whether or not the file should be considered temporary.
+     */
+    public function setIsTempFile($isTempFile)
+    {
+        $this->isTempFile = (bool) $isTempFile;
+    }
 
-	/**
-	 * Simply return whether or not the given file's syntax is valid.
-	 *
-	 * @return bool True if validation passes, false if an error is encountered.
-	 */
-	public function isValid()
-	{
-		try {
-			$this->validate();
-		} catch (ValidationException $e) {
-			return false;
-		}
+    /**
+     * Simply return whether or not the given file's syntax is valid.
+     *
+     * @return bool True if validation passes, false if an error is encountered.
+     */
+    public function isValid()
+    {
+        try {
+            $this->validate();
+        } catch (ValidationException $e) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Validate the given file.
-	 *
-	 * Validation is handled by the underlying Apache instance, using a stripped-down configuration
-	 * so the entire Apache configuration consists of a) the necessary bootstrapping and b) the
-	 * contents of $this->file.
-	 *
-	 * @throws ValidationException
-	 */
-	public function validate()
-	{
-		$command     = sprintf(
-			'%1$s %2$s',
-			escapeshellarg(dirname(__DIR__) . '/bin/validate-htaccess'),
-			escapeshellarg($this->file)
-		);
+    /**
+     * Validate the given file.
+     *
+     * Validation is handled by the underlying Apache instance, using a stripped-down configuration
+     * so the entire Apache configuration consists of a) the necessary bootstrapping and b) the
+     * contents of $this->file.
+     *
+     * @throws ValidationException
+     */
+    public function validate()
+    {
+        $command     = sprintf(
+            '%1$s %2$s',
+            escapeshellarg(dirname(__DIR__) . '/bin/validate-htaccess'),
+            escapeshellarg($this->file)
+        );
         $descriptors = [
             0 => ['pipe', 'r'],
             1 => ['pipe', 'w'],
             2 => ['pipe', 'w'],
         ];
 
-		$process = proc_open($command, $descriptors, $pipes);
+        $process = proc_open($command, $descriptors, $pipes);
 
         if (! is_resource($process)) {
             throw new ValidationException('Unable to open validation process.');
@@ -102,27 +102,27 @@ class Validator
 
         $exitCode = proc_close($process);
 
-		if (0 !== $exitCode) {
-			throw new ValidationException(
-				sprintf('Validation errors were encountered: %s', $pipes[2]),
-				$exitCode
-			);
-		}
-	}
+        if (0 !== $exitCode) {
+            throw new ValidationException(
+                sprintf('Validation errors were encountered: %s', $pipes[2]),
+                $exitCode
+            );
+        }
+    }
 
-	/**
-	 * Create a new validator instance using a temporary file.
-	 *
-	 * @return self
-	 */
-	public static function createFromString($contents)
-	{
-		$file = tempnam(null, 'htaccess-');
-		file_put_contents($file, $contents);
+    /**
+     * Create a new validator instance using a temporary file.
+     *
+     * @return self
+     */
+    public static function createFromString($contents)
+    {
+        $file = tempnam(null, 'htaccess-');
+        file_put_contents($file, $contents);
 
-		$instance = new static($file);
-		$instance->setIsTempFile(true);
+        $instance = new static($file);
+        $instance->setIsTempFile(true);
 
-		return $instance;
-	}
+        return $instance;
+    }
 }
